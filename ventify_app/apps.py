4c6ed91e7ceda7d07a8efccf603cf4b64747bfb8
TRVPMAX9 <trvpmax9@proton.me>
2024-07-24 11:37:58 +0300

    Configure app settings in VentifyAppConfig
    - Set default_auto_field to BigAutoField.
    - Specify app name and default configuration for ventify_app.

---
+++ ventify_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VentifyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ventify_app'
+    default_config = 'ventify_app'
